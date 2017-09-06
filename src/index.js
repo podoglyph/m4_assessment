06bf22e1595349a49312c58f984de1287c8f6d2f
Padraic O'Neill <pconeill@gmail.com [core]>
2017-09-05 20:17:15 -0600

    removes comment, completes first user story

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const tw = $('.top-word h3')
 
   $.getJSON(API + `/api/v1/top_word`, function(data) {
-    // getJSON( url [, data ] [, success ] )
     let word = Object.keys(data.word)[0]
     let number = Object.values(data.word)[0]
     currentText = tw.text()
