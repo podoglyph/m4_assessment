be59d9e77509d9482a32ba3769e50592e4cb191c
Padraic O'Neill <pconeill@gmail.com [core]>
2017-09-05 20:16:26 -0600

    adds word and count to top-word heading

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const $ = require('jquery')
 const API = "https://wordwatch-api.herokuapp.com"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const tw = $('.top-word')
+  const tw = $('.top-word h3')
 
   $.getJSON(API + `/api/v1/top_word`, function(data) {
     // getJSON( url [, data ] [, success ] )
     let word = Object.keys(data.word)[0]
     let number = Object.values(data.word)[0]
-    console.log(word, number)
+    currentText = tw.text()
+    tw.text(`${currentText} ${word} (${number})`)
   })
 
 })
